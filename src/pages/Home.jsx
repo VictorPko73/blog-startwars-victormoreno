4cb07b3f9ee58d08a8aaec2e347640f56fe847b8
Victor Moreno Cabello <victormorenocabello@gmail.com>
2025-09-04 14:18:16 +0200

    Finish

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,6 @@
 
-
-
 import { Image, useEffect } from "react";
 import { Link } from "react-router-dom";
-
-
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 // Funciones para localStorage favoritos
